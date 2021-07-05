44e99e9e9f72166fcba4fc132e42a8d82b61ee5c
Mael Galite <mgalite@google.com>
2021-07-05 15:06:35 +0200

    Allow tfstate to be saved in a backend

--- 0-bootstrap/outputs.tf
+++ 0-bootstrap/outputs.tf
@@ -5,3 +5,13 @@ output "project_id" {
 output "bootstrap_bucket" {
   value = module.seed_bootstrap.gcs_bucket_tfstate
 }
+
+output "terraform_sa_email" {
+  description = "Email for privileged service account for Terraform."
+  value       = module.seed_bootstrap.terraform_sa_email
+}
+
+output "terraform_sa_name" {
+  description = "Fully qualified name for privileged service account for Terraform."
+  value       = module.seed_bootstrap.terraform_sa_name
+}
