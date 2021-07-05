7f6df8f9240e4fb246d084aebfabc56dc53a9a8d
Mael Galite <mgalite@google.com>
2021-07-05 15:19:32 +0200

    Bump tf to 1.0.1

--- 0-bootstrap/main.tf
+++ 0-bootstrap/main.tf
@@ -15,13 +15,17 @@ module "cloudbuild_bootstrap" {
   source  = "terraform-google-modules/bootstrap/google//modules/cloudbuild"
   version = "~> 2.1"
 
-  org_id                  = var.organization_id
-  billing_account         = var.billing_account
-  group_org_admins        = var.group_org_admins
-  default_region          = var.default_region
-  sa_enable_impersonation = true
-  terraform_sa_email      = module.seed_bootstrap.terraform_sa_email
-  terraform_sa_name       = module.seed_bootstrap.terraform_sa_name
-  terraform_state_bucket  = module.seed_bootstrap.gcs_bucket_tfstate
-  project_prefix          = var.project_prefix
+  org_id                      = var.organization_id
+  billing_account             = var.billing_account
+  group_org_admins            = var.group_org_admins
+  default_region              = var.default_region
+  sa_enable_impersonation     = true
+  terraform_sa_email          = module.seed_bootstrap.terraform_sa_email
+  terraform_sa_name           = module.seed_bootstrap.terraform_sa_name
+  terraform_state_bucket      = module.seed_bootstrap.gcs_bucket_tfstate
+  project_prefix              = var.project_prefix
+  cloud_source_repos          = ["gcp-projects"]
+  terraform_apply_branches    = ["master", "main"]
+  terraform_version           = "1.0.1"
+  terraform_version_sha256sum = "da94657593636c8d35a96e4041136435ff58bb0061245b7d0f82db4a7728cef3"
 }
--- 0-bootstrap/outputs.tf
+++ 0-bootstrap/outputs.tf
@@ -15,3 +15,8 @@ output "terraform_sa_name" {
   description = "Fully qualified name for privileged service account for Terraform."
   value       = module.seed_bootstrap.terraform_sa_name
 }
+
+output "csr_repos" {
+  description = "List of Cloud Source Repos created by the module, linked to Cloud Build triggers."
+  value       = module.cloudbuild_bootstrap.csr_repos
+}
