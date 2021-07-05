4088dcdc467f5c610f0fb36ab1cc79685fc645e2
Mael Galite <mgalite@google.com>
2021-07-05 16:42:08 +0200

    Prepare for factory project

---
+++ 1-factory/variables.tf
@@ -0,0 +1,9 @@
+variable "organization_id" {
+  description = "The organization ID."
+  type        = string
+}
+
+variable "billing_account" {
+  description = "The ID of the billing account to associate this project with"
+  type        = string
+}
